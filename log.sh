1f430b2d8d5aad7cbb4df97418db0685a451765d
huichen <343063670@qq.com>
2016-04-30 13:30:47 +0800

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Author: huichen
+# Date  : 2016-04-30
+# Usage : source /path/to/log.sh
+#         log level "strings"
+
+function log()
+{
+        local level=$1
+        shift
+        local msg=$*
+        case $level in
+                info)   echo -e "$ts [INFO] $msg";;
+                note)   echo -e "$ts [NOTE] \033[92m$msg\033[0m";;
+                warn)   echo -e "$ts [WARN] \033[93m$msg\033[0m";;
+                error)  echo -e "$ts [ERROR] \033[91m$msg\033[0m";;
+        esac
+}
